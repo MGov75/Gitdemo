d769c1201fe75b6aebd67f43215c9f7cda001d07
MGov75 <88401110+MGov75@users.noreply.github.com>
2021-08-10 18:13:47 +0100

    Update verifyTitle.java

--- src/Google/verifyTitle.java
+++ src/Google/verifyTitle.java
@@ -5,6 +5,7 @@ public class verifyTitle {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	System.out.println("Verify title..");
+	System.out.println("Verify new and old title..");	
 
 	}
 
